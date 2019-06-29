c5bce215462119dde3d15b559269736347a32ae0
Vladimir Masarik <masarik.vladimir7@gmail.com>
2019-06-29 10:10:16 +0200

    removed db and commented models

--- gitistics/models.py
+++ gitistics/models.py
@@ -7,10 +7,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class UserProfileInfo(models.Model):
+# class UserProfileInfo(models.Model):
 
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='avatars',blank=True)
+#     user = models.OneToOneField(User,on_delete=models.CASCADE)
+#     profile_pic = models.ImageField(upload_to='avatars',blank=True)
 
-    def __str__(self):
-        return self.user.username
+#     def __str__(self):
+#         return self.user.username
