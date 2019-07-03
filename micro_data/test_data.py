3076a2ee65764a69d5846664d7075921f942a72a
Vladimir Masarik <masarik.vladimir7@gmail.com>
2019-07-03 08:26:53 +0200

    removed empty string test

--- micro_data/test_data.py
+++ micro_data/test_data.py
@@ -5,10 +5,6 @@ import pytest, unittest, requests, logging
 
 import micro_data.main
 
-def test_type_data_removeEmptyString():
-    assert(type(micro_data.main.removeEmptyString({"key": ""})) == dict)
-    assert(type(micro_data.main.removeEmptyString({"key": "value"})) == dict)
-
 def test_type_data_userRepos():
     assert(type(micro_data.main.userRepos("vladmasarik")) == list)
 
