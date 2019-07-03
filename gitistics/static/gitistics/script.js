94997eb6dd207462a425b631757cc43629b75776
Vanacerneka <vana.cerneka@fer.hr>
2019-07-03 11:31:14 +0200

    finished summary

--- gitistics/static/gitistics/script.js
+++ gitistics/static/gitistics/script.js
@@ -18,21 +18,114 @@ function regUsers() {
     }
   });
 }
-window.onload = regUsers;
 
 
 // Number of languages
 function noOfLangs() {
   const urlParams = new URLSearchParams(window.location.search);
   const search = urlParams.get('search_term');
-  $.ajax({
-    url: `/api/languageList?search_term=${search}`,
-    success: function (result) {
-      document.getElementById("showNoOfLangs").innerHTML += Object.keys(result["languageList"]).length;
-    }
-  });
+  if (search !== null && search !== '') {
+    $.ajax({
+      url: `/api/languageList?search_term=${search}`,
+      success: function (result) {
+        document.getElementById("showNoOfLangs").innerHTML = Object.keys(result["languageList"]).length;
+      }
+    });
+  }
 }
-window.onload = noOfLangs;
+
+
+// Number of forks
+function noOfForks() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const search = urlParams.get('search_term');
+  if (search !== null && search !== '') {
+    $.ajax({
+      url: `/api/repoList?search_term=${search}`,
+      success: function (result) {
+        list = result["list"]
+        repos = list["repositories"]
+        count = 0
+        repos.forEach(function (repo) {
+          count += repo['forks_count']
+        });
+        document.getElementById("showNoOfForks").innerHTML = count;
+      }
+    });
+  }
+}
+
+
+// Number of issues
+function noOfIssues() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const search = urlParams.get('search_term');
+  if (search !== null && search !== '') {
+    $.ajax({
+      url: `/api/repoList?search_term=${search}`,
+      success: function (result) {
+        list = result["list"]
+        repos = list["repositories"]
+        count = 0
+        repos.forEach(function (repo) {
+          count += repo['open_issues_count']
+        });
+        document.getElementById("showNoOfIssues").innerHTML = count;
+      }
+    });
+  }
+}
+
+
+// Number of watchers
+function noOfWatchers() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const search = urlParams.get('search_term');
+  if (search !== null && search !== '') {
+    $.ajax({
+      url: `/api/repoList?search_term=${search}`,
+      success: function (result) {
+        list = result["list"]
+        repos = list["repositories"]
+        count = 0
+        repos.forEach(function (repo) {
+          count += repo['watchers_count']
+        });
+        document.getElementById("showNoOfWatchers").innerHTML = count;
+      }
+    });
+  }
+}
+
+
+// Number of stars
+function noOfStars() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const search = urlParams.get('search_term');
+  if (search !== null && search !== '') {
+    $.ajax({
+      url: `/api/repoList?search_term=${search}`,
+      success: function (result) {
+        list = result["list"]
+        repos = list["repositories"]
+        count = 0
+        repos.forEach(function (repo) {
+          count += repo['stargazers_count']
+        });
+        document.getElementById("showNoOfStars").innerHTML = count;
+      }
+    });
+  }
+}
+
+window.onload = function () {
+  regUsers()
+  noOfIssues()
+  noOfWatchers()
+  noOfForks()
+  noOfLangs()
+  noOfStars()  
+};
 
 
 // Piechart for languages
@@ -56,7 +149,7 @@ document.getElementById("langButton").addEventListener("click", function () {
         var data = google.visualization.arrayToDataTable(list);
 
         var options = {
-          height: 300
+          height: 400
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -91,9 +184,9 @@ document.getElementById("reposButton").addEventListener("click", function () {
     cells[3] = firstrow.insertCell(3);
     cells[4] = firstrow.insertCell(4);
 
-    cells[0].style.width = "40%";
-    cells[1].style.width = "15%";
-    cells[2].style.width = "15%";
+    cells[0].style.width = "30%";
+    cells[1].style.width = "20%";
+    cells[2].style.width = "20%";
     cells[3].style.width = "15%";
     cells[4].style.width = "15%";
 
@@ -111,36 +204,34 @@ document.getElementById("reposButton").addEventListener("click", function () {
       url: `/api/repoList?search_term=${searchRepo}`,
       success: function (result) {
         list = result["list"]
-        list.forEach(function (repo) {
+        repos = list["repositories"]
+        repos.forEach(function (repo) {
 
           var tableBody = document.getElementById("timestamp");
           var row = document.createElement("tr");
 
           var reponame = document.createElement("td");
-          //name {{ repo.name }}
-          reponame.appendChild(document.createTextNode(repo));
+          reponame.appendChild(document.createTextNode(repo["name"]));
 
           var created = document.createElement("td");
           created.style.textAlign = "center";
-          //createdtime {{ repo.created_at }}
-          var d = new Date(parseInt(repo.created_at, 10));
+          var d = new Date(repo["created_at"]);
           var ds = d.toLocaleString("en-GB");
           created.appendChild(document.createTextNode(ds));
 
           var pushed = document.createElement("td");
           pushed.style.textAlign = "center";
-          //pushedtime {{ repo.pushed_at }}
-          var d = new Date(parseInt(repo.pushed_at, 10));
+          var d = new Date(repo["pushed_at"]);
           var ds = d.toLocaleString("en-GB");
           pushed.appendChild(document.createTextNode(ds));
 
           var language = document.createElement("td");
-          //language {{ repo.language }}
-          language.appendChild(document.createTextNode(repo));
+          language.style.textAlign = "center";
+          language.appendChild(document.createTextNode(repo["language"]));
 
           var size = document.createElement("td");
-          //size {{ repo.size }}
-          size.appendChild(document.createTextNode(repo.size));
+          size.style.textAlign = "center";
+          size.appendChild(document.createTextNode(repo["size"] + ' KB'));
 
           row.appendChild(reponame);
           row.appendChild(created);
